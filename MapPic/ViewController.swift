6222bdfd821faaf608df90b6735593b5a57173ac
niemczus <niemczus@gmail.com>
2022-02-25 15:19:51 +0100

    First commit

---
+++ MapPic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapPic
+//
+//  Created by Kamil Niemczyk on 25/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
